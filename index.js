e2f70ec14295f3a3deb803ee204cc82a9bf9ade7
Jin <305648262@qq.com>
2016-12-04 19:11:42 +0800

    update demo

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 $(function(){
+    // demo1
    var str="<p>This is a popupbox automatically generated</p>";
    $.jCreateForm({
        'tag_id':'hahaha',
@@ -10,6 +11,18 @@ $(function(){
                alert(p);
            },
            'ok_func_param':"Hello,Jin"
+       });
+   });
+   //demo2
+   $("#show_2").click(function(){
+       $("#form_2").jShow({
+           'close_mode':2,
+           'close_tag':'#demo-2-close',
+           'ok_btn':'#demo-2-ok',
+           'ok_func':function(p){
+               alert(p);
+           },
+           'ok_func_param':"This is Demo2."
        });
    });
 });
--- md/md.js
+++ md/md.js
@@ -1,6 +1,12 @@
 /*jshint multistr: true */
 (function(){
-    var code_block_1="```\n $(function(){\n\
+var code_test_1="```"+
+    "$a=123;\n"+
+    "function abc(){\n"+
+    "}\n"+
+    "```";
+var code_block_1="```\n\
+$(function(){\n\
     var str=\"<p>This is a popupbox automatically generated</p>\";\n\
     $.jCreateForm({\n\
         'tag_id':'hahaha',\n\
@@ -14,6 +20,29 @@
             'ok_func_param':\"Hello,Jin\"\n\
         });\n\
     });\n\
-});\n```";
-    $("#code-block-1").html(markdown.toHTML(code_block_1));
+});\n\
+```";
+
+var code_block_2="```\n\
+<!-- html code -->\n\
+<div id=\"form_2\" class=\"demo-box-2\"  hidden=\"hidden\">\n\
+    <button id=\"demo-2-ok\">Ok</button>\n\
+<button style=\"float:right\" id=\"demo-2-close\">Close</button><br>\n\
+</div>\n\
+//js code\n\
+$(\"#show_2\").click(function(){\n\
+    $(\"#form_2\").jShow({\n\
+        'close_mode':2,\n\
+        'close_tag':'#demo-2-close',\n\
+        'ok_btn':'#demo-2-ok',\n\
+        'ok_func':function(p){\n\
+            alert(p);\n\
+        },\n\
+        'ok_func_param':\"This is Demo2.\"\n\
+    });\n\
+});\n\
+```";
+
+$("#code-block-1").html(markdown.toHTML(code_block_1));
+$("#code-block-2").html(markdown.toHTML(code_block_2));
 })();
